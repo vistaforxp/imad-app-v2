73266b7b3ff1e74fc2ed5e26fd0b5d59422697f2
vistaforxp <sunny.mtrix@gmail.com>
2017-03-20 09:11:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-var articleOne = {
+var articles = {
+'article-one' :{
     title :'Article One | Gaurav Arora',
     heading:'Article One',
     date: 'March 20, 2017',
@@ -18,6 +18,27 @@ var articleOne = {
                     
                 </p>
     `
+},
+'article-two':{title :'Article Two | Gaurav Arora',
+    heading:'Article Two',
+    date: 'March 21, 2017',
+    content:`<p>
+                    This is my second article. Content of my first article are here
+                    
+                </p>
+                <p>
+                    This is my 2nd article. Content of my first hello hello article are here
+                    
+                </p>
+    `},
+'article-three':{title :'Article Three | Gaurav Arora',
+    heading:'Article Three',
+    date: 'March 22, 2017',
+    content:` <p>
+                    
+                 Oblivion's main story revolves around the player character's efforts to thwart a fanatical cult known as the "Mythic Dawn" that plans to open the gates to a realm called "Oblivion". The game continues the open world tradition of its predecessors by allowing the player to travel anywhere in the game world at any time and to ignore or postpone the main storyline indefinitely. A perpetual objective for players is to improve their character's skills, which are numerical representations of certain abilities. Seven skills are selected early in the game as major skills, with the remainder termed minor. Developers opted for tighter pacing in gameplay and greater plot focus than in past titles.                
+                </p>
+    `}
 };
 function createTemplate (data){
     var title = data.title;
@@ -64,19 +85,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res)
+app.get('/articleName', function(req,res)
+// articleOne ==article-one
+//articles[articleName] == {} content object for article one
     {
-    res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two', function(req,res)
-    {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res)
-    {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
