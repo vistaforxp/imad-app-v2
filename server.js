de0f189da4399f55356654c442fac4e8985738ad
vistaforxp <sunny.mtrix@gmail.com>
2017-03-20 07:54:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function(req,res)
+    {
+    res.send('Article one requested and will be served');
+});
+
+app.get('/article-two', function(req,res)
+    {
+    res.send('Article two requested and will be served');
+});
+
+app.get('/article-three', function(req,res)
+    {
+    res.send('Article three requested and will be served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
