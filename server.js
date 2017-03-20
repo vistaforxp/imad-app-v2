e00684c836a484f78c10b3e569992de93f76fa91
vistaforxp <sunny.mtrix@gmail.com>
2017-03-20 08:52:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,68 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title :'Article One | Gaurav Arora',
+    heading:'Article One',
+    date: 'March 20, 2017',
+    content:` <p>
+                    This is my first article. Content of my first article are here
+                    
+                </p>
+                <p>
+                    This is my first article. Content of my first hello hello article are here
+                    
+                </p>
+    `
+};
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name ="viewport" content="width=device-width,initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class ="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+        
+        
+    </html>
+    
+    
+    `;
+    
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res)
     {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req,res)
