f685964320a5d3ca471d83276cd56e58b0814276
vistaforxp <sunny.mtrix@gmail.com>
2017-03-21 01:01:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,7 @@ var element = document.getElementById('main-text');
 element.innerHTML="New Value";
 
 //move the image
-document.getElementById('madi').onclick=function(){
-    madi.style.marginLeft = "100px";
+var img= document.getElementById('madi')
+img.onclick=function(){
+    img.style.marginLeft = "100px";
 }
