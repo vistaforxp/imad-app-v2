ab12857b30766095f51af07a9e17a8ed130c82c2
vistaforxp <sunny.mtrix@gmail.com>
2017-03-21 00:55:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 element.innerHTML="New Value";
+
+//move the image
+document.getElementById('img').onclick=function(){
+    img.style.marginLeft = "100px";
+}
